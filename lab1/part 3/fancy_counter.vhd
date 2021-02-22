8826c801d04309e2e190297b9f25b5c6bf0145f4
mmo91 <46290306+mmo91@users.noreply.github.com>
2021-02-22 22:22:16 +0000

    Update fancy_counter.vhd

--- lab1/part 3/fancy_counter.vhd
+++ lab1/part 3/fancy_counter.vhd
@@ -51,6 +51,7 @@ signal counter : std_logic_vector (3 downto 0) := (others => '0');
 
 begin
     process(clk)
+
         begin
             if (rising_edge(clk)) then
                 if (en = '0') then -- enables the circuit to perform
@@ -62,27 +63,26 @@ begin
                     if (updn = '1') then
                         direction <= dir;
                     end if;
-               -- checks to see if the counter reached max val --
-                 if (unsigned(counter) = unsigned(value)) then
+                   -- checks to see if the counter reached max val --
+                    if (unsigned(counter) = unsigned(value)) then
                         counter <= (others => '0');
                         cnt <= (others => '0');
-                 else
-                 -- sets the counter value -- 
-                    if (direction = '1') then
-                        counter <= std_logic_vector(unsigned(counter) + 1);
-                        cnt <= std_logic_vector(unsigned(counter) + 1);
                     else
-                        counter <= std_logic_vector(unsigned(counter) - 1);
-                        cnt <= std_logic_vector(unsigned(counter) - 1);
-                    end if;
+                     -- sets the counter value -- 
+                        if (direction = '1') then
+                            counter <= std_logic_vector(unsigned(counter) + 1);
+                            cnt <= std_logic_vector(unsigned(counter) + 1);
+                        else
+                            counter <= std_logic_vector(unsigned(counter) - 1);
+                            cnt <= std_logic_vector(unsigned(counter) - 1);
+                        end if;
 
                     end if;                                     
-                else if (clk_en = '0') then -- if en = '1' and clk_en = '0'
+                elsif (clk_en = '0') then -- if en = '1' and clk_en = '0'
                     if (rst = '1') then
                         cnt <= (others => '0');
                     end if;
                 end if;
-            end if;
             end if;
         end process;
 
