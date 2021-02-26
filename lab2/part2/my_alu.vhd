304f867f735ebe4d6cbbe25cc00f93417ff22359
mmo91 <46290306+mmo91@users.noreply.github.com>
2021-02-26 15:27:13 -0500

    Update my_alu.vhd

--- lab2/part2/my_alu.vhd
+++ lab2/part2/my_alu.vhd
@@ -12,9 +12,9 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity my_alu is
- Port (
-        A, B, Opcode : in unsigned;
-        funct : out unsigned
+ Port (clk : in std_logic;
+        A, B, Opcode : in unsigned (3 downto 0);
+        funct : out unsigned (3 downto 0)
   );
 end my_alu;
 
@@ -24,11 +24,11 @@ signal op : std_logic_vector (3 downto 0) := std_logic_vector(Opcode);
 signal hd: std_logic_vector (1 downto 0);
 
 begin
-process
+process(clk)
 begin
 
-
-case (op) is
+if rising_edge(clk) then
+case (Opcode) is
     when "0000" =>
         funct <= A + B;
     when "0001" =>
@@ -40,31 +40,35 @@ case (op) is
     when "0100" =>
         funct <= 0 - A;
     when "0101" =>
-        -- 
+        if (A > B) then 
+            funct <= To_unsigned(1,4);
+        else
+            funct <= To_unsigned(0,4);
+        end if;
     when "0110" =>
         funct <= unsigned( A sll 1);
     when "0111" =>
         funct <= unsigned (A srl 1);
     when "1000" =>
-    
+        funct <= shift_right(A,1);
     when "1001" =>
-    
+        funct <= unsigned(not std_logic_vector(A));
     when "1010" => 
-    
+        funct <= unsigned(std_logic_vector(A) and std_logic_vector(B));
     when "1011" =>
-    
+        funct <= unsigned(std_logic_vector(A) or std_logic_vector(B));
     when "1100" =>
-    
+        funct <= unsigned(std_logic_vector(A) xor std_logic_vector(B));
     when "1101" =>
-    
+        funct <= unsigned(std_logic_vector(A) xnor std_logic_vector(B));
     when "1110" =>
-    
+        funct <= unsigned(std_logic_vector(A) nand std_logic_vector(B));
     when "1111" =>
-    
-        
-    when others =>
-        
+        funct <= unsigned(std_logic_vector(A) nor std_logic_vector(B)); 
+    when others => 
+        funct <= To_unsigned(0,4);   
  end case;
+ end if;
         
 
 
